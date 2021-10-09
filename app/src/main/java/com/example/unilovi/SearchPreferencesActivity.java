b63f3563c589b8c75cf4a70b23fd0b1800928f03
uo271397 <uo271397@uniovi.es>
2021-10-09 18:51:46 +0200

    Activity de preferencias de busqueda (no finalizado)

---
+++ app/src/main/java/com/example/unilovi/SearchPreferencesActivity.java
@@ -0,0 +1,130 @@
+package com.example.unilovi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchPreferencesActivity extends AppCompatActivity {
+
+    private SeekBar seekBarMinima;
+    private SeekBar seekBarMaxima;
+    private TextView edadMinima;
+    private TextView edadMaxima;
+    private CheckBox checkHombre;
+    private CheckBox checkMujer;
+    private Button btnGuardarPreferencias;
+    private List<String> listaFacultades;
+    private Spinner spinnerFacultades;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_preferences);
+
+        seekBarMinima = (SeekBar) findViewById(R.id.seekBarEdadMinima);
+        edadMinima = findViewById(R.id.edadMinima);
+
+        edadMinima.setText("17");
+
+        seekBarMaxima = findViewById(R.id.seekBarEdadMaxima);
+        edadMaxima = findViewById(R.id.edadMaxima);
+
+        edadMaxima.setText("50");
+
+        seekBarMinima.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int edad = i + 17;
+                edadMinima.setText(edad + "");
+                if (Integer.parseInt(edadMinima.getText().toString()) >= Integer.parseInt(edadMaxima.getText().toString())) {
+                    int edad2 = Integer.parseInt(edadMinima.getText().toString()) + 1;
+                    edadMaxima.setText(edad2 + "");
+                    seekBarMaxima.setProgress(seekBarMinima.getProgress() + 1);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBarMaxima.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int edad = i + 18;
+                edadMaxima.setText(edad + "");
+                if (Integer.parseInt(edadMaxima.getText().toString()) <= Integer.parseInt(edadMinima.getText().toString())) {
+                    int edad2 = Integer.parseInt(edadMaxima.getText().toString()) - 1;
+                    edadMinima.setText(edad2 + "");
+                    seekBarMinima.setProgress(seekBarMaxima.getProgress() - 1);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        listaFacultades = new ArrayList<String>();
+        listaFacultades.add("Sin definir");
+        listaFacultades.add("Facultad de ciencias");
+        listaFacultades.add("Facultad de medicina");
+        listaFacultades.add("...");
+
+        spinnerFacultades = findViewById(R.id.spinnerFacultades);
+
+        rellenarSpinner(spinnerFacultades, listaFacultades);
+
+        checkHombre = findViewById(R.id.checkHombre);
+        checkMujer = findViewById(R.id.checkMujer);
+
+        btnGuardarPreferencias = findViewById(R.id.btnGuardarPreferencias);
+
+        btnGuardarPreferencias.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkHombre.isChecked() || checkMujer.isChecked()) {
+                    Intent intentResultado = new Intent();
+                    setResult(RESULT_OK, intentResultado);
+                    finish();
+                } else {
+                    Snackbar.make(findViewById(R.id.seekBarMinima), R.string.no_guardado,
+                            Snackbar.LENGTH_SHORT)
+                            .show();
+                }
+            }
+        });
+    }
+
+    public void rellenarSpinner(Spinner spinner, List<String> array) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, array);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+}
--- app/src/main/java/com/example/unilovi/ThreeButtonActivity.java
+++ app/src/main/java/com/example/unilovi/ThreeButtonActivity.java
@@ -2,6 +2,7 @@ package com.example.unilovi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,7 +28,8 @@ public class ThreeButtonActivity extends AppCompatActivity {
         btnPreferencias.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent preferencesIntent = new Intent(ThreeButtonActivity.this, SearchPreferencesActivity.class);
+                startActivity(preferencesIntent);
             }
         });
 

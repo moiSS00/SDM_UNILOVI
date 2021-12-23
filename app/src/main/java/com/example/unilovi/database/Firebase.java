4012cb670c585c1de6af8008267f2b5f8704b5cd
Moisés Sanjurjo Sánchez <71430057+moiSS00@users.noreply.github.com>
2021-12-23 19:22:00 +0100

    Organización de la clase Firebase

--- app/src/main/java/com/example/unilovi/database/Firebase.java
+++ app/src/main/java/com/example/unilovi/database/Firebase.java
@@ -33,14 +33,111 @@ public class Firebase {
     // Base de datos
     private static FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-    // Modulo de autentificación
+    // Módulo de autentificación
     private static FirebaseAuth fAuth = FirebaseAuth.getInstance();
 
-    //Storage de fotos
+    // Módulo de multimedia
     private static StorageReference storage = FirebaseStorage.getInstance().getReference();
 
 
     // ---- Autenticación ---
+
+    /**
+     * Registra a un usuario SOLO en el módulo de autenticación
+     * @param email Email del usuario a registrar
+     * @param password Contraseña del usuario a registrar
+     * @param callBack CallBack a ejecutar
+     */
+    public static void registrarUsuario(String email, String password, CallBack callBack) {
+        fAuth.createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        callBack.methodToCallBack(true);
+                    }})
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        callBack.methodToCallBack(false);
+                    }
+                });
+    }
+
+    /**
+     * Crea un usuario SOLO en la base de datos (NO EN EL SERVICIO DE AUTENTIFICACIÓN)
+     * @param user Usuario a crear
+     * @param preferences Preferencias del usuario a crear
+     * @param uriFoto La uri que indica donde se encuentra la foto de perfil del usuario a crear
+     * @param callBack CallBack a ejecutar
+     */
+    public static void createUser(User user, Preferences preferences, String uriFoto, CallBack callBack) {
+
+        // Create a new user
+        Map<String, Object> userParams = new HashMap<>();
+        userParams.put("nombre", user.getNombre());
+        userParams.put("apellidos", user.getApellidos());
+        userParams.put("fechaNacimiento", user.getFechaNacimiento());
+        userParams.put("sexo", user.getSexo());
+        userParams.put("facultad", user.getFacultad());
+        userParams.put("carrera", user.getCarrera());
+        userParams.put("sobreMi", user.getSobreMi());
+        userParams.put("contacto", user.getFormaContacto());
+
+        // Create preferences for user
+        Map<String, Object> userPreferences = new HashMap<>();
+        userPreferences.put("edadMinima", preferences.getEdadMinima());
+        userPreferences.put("edadMaxima", preferences.getEdadMaxima());
+        userPreferences.put("sexoBusqueda", preferences.getSexos());
+        userPreferences.put("facultad", preferences.getFacultad());
+        userPreferences.put("carrera", preferences.getCarrera());
+
+        // Añadimos al usuario
+        db.collection("usuarios").document(user.getEmail()).set(userParams)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        db.collection("preferencias").document(user.getEmail()).set(userPreferences)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        uploadImage(Uri.parse(uriFoto));
+                                        callBack.methodToCallBack(true);
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                callBack.methodToCallBack(false);
+                            }
+                        });
+                    }})
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        callBack.methodToCallBack(false);
+                    }
+                });
+    }
+
+    /**
+     * Método para iniciar sesión
+     * @param email Email del usuario
+     * @param password Contraseña del usuario
+     * @param callBack CallBack a ejecutar
+     */
+    public static void iniciarSesion(String email, String password, CallBack callBack) {
+        fAuth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        callBack.methodToCallBack(true);
+                    }})
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        callBack.methodToCallBack(false);
+                    }
+                });
+    }
 
     /**
      * Cierra la sesión del usuario actual
@@ -58,30 +155,8 @@ public class Firebase {
     }
 
     /**
-     * Método para iniciar sesión
-     * @param email Email del usuario
-     * @param password Contraseña del usuario
-     * @param callBack CallBack a ejecutar, recibirá true si no hubo errores  o
-     * false si hubo algún error.
-     */
-    public static void iniciarSesion(String email, String password, CallBack callBack) {
-        fAuth.signInWithEmailAndPassword(email, password)
-                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-            @Override
-            public void onSuccess(AuthResult authResult) {
-                callBack.methodToCallBack(true);
-            }})
-                .addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                callBack.methodToCallBack(false);
-            }
-        });
-    }
-
-    /**
-     * Comprueba si es la primera vez que inicia sesion
-     * @param callBack
+     * Comprueba si es la primera vez que el usuario actual inicia sesion
+     * @param callBack Callback a ejecutar
      */
     public static void existUser(CallBack callBack) {
         db.collection("usuarios").document(getUsuarioActual().getEmail()).get()
@@ -103,115 +178,10 @@ public class Firebase {
                 });
     }
 
-    /**
-     * Registra un usuario en la base de datos
-     * @param email Email del usuario a registrar
-     * @param password Contraseña del usuario
-     * @param callBack CallBack a ejecutar, recibirá true si no hubo errores  o
-     * false si hubo algún error.
-     */
-    public static void registrarUsuario(String email, String password, CallBack callBack) {
-        fAuth.createUserWithEmailAndPassword(email, password)
-                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-            @Override
-            public void onSuccess(AuthResult authResult) {
-                callBack.methodToCallBack(true);
-            }})
-                .addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                callBack.methodToCallBack(false);
-            }
-        });
-    }
-
-
     // ---- Interactuar directamente con la base de datos ----
 
     /**
-     * Devuelve la lista de ciudades almacenadas en la base de datos
-     * @param callBack CallBack a ejecutar, recibirá la lista de ciudades si no hubo errores  o
-     * null si hubo algún error.
-     */
-    public static void getCiudades(CallBack callBack) {
-        db.collection("ciudades").document("ciudadesFormularioUsuario").get()
-                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                if (documentSnapshot.exists()) { // Si se encontró el documento que contiene a las ciudades
-                    callBack.methodToCallBack(documentSnapshot.getData().get("Ciudades"));
-                }
-                else {
-                    callBack.methodToCallBack(null);
-                }
-            }})
-                .addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                callBack.methodToCallBack(null);
-            }
-        });
-    }
-
-    /**
-     * Devuelve la lista de facultades almacenadas en la base de datos
-     * @param callBack CallBack a ejecutar, recibirá la lista de facultades si no hubo errores  o
-     * null si hubo algún error.
-     */
-    public static void getFacultades(CallBack callBack) {
-        db.collection("facultades").get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) { // Si se ha logrado recolectar todos los documentos de las facultades
-                            List<String> facultades = new ArrayList<String>();
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                // Para cada documento obtenemos su id (nombre de la facultad)
-                                facultades.add(document.getId());
-                            }
-                            callBack.methodToCallBack(facultades);
-                        }
-                        else {
-                            callBack.methodToCallBack(null);
-                        }
-                    }})
-                .addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                callBack.methodToCallBack(null);
-            }
-        });
-    }
-
-    /**
-     * Devuelve la lista de carreras que se dan en una facultad específica
-     * @param facultad Facultad por la que se quiere filtrar
-     * @param callBack CallBack a ejecutar, recibirá la lista de carreras si no hubo errores  o
-     * null si hubo algún error.
-     */
-    public static void getCarrerasByFacultad(String facultad, CallBack callBack) {
-        // Buscamos el documento para esa facultad
-        db.collection("facultades").document(facultad).get()
-                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                if (documentSnapshot.exists()) { // Si existe la facultad
-                    callBack.methodToCallBack(documentSnapshot.getData().get("carreras"));
-                }
-                else { // Si no existe la facultad mostramos valor por defecto
-                    callBack.methodToCallBack(null);
-                }
-            }})
-                .addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                callBack.methodToCallBack(null);
-            }
-        });
-    }
-
-    /**
-     * Devuelve el nombre del usuario para darle la bienvenida
+     * Devuelve la información de un usuario en concreto
      * @param email Email del usuario
      */
     public static void getUsuarioByEmail(String email, CallBack callBack) {
@@ -247,7 +217,86 @@ public class Firebase {
     }
 
     /**
-     * Devuelve las preferencias del usuario
+     * Devuelve la lista de ciudades almacenadas en la base de datos
+     * @param callBack CallBack a ejecutar
+     */
+    public static void getCiudades(CallBack callBack) {
+        db.collection("ciudades").document("ciudadesFormularioUsuario").get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()) { // Si se encontró el documento que contiene a las ciudades
+                    callBack.methodToCallBack(documentSnapshot.getData().get("Ciudades"));
+                }
+                else {
+                    callBack.methodToCallBack(null);
+                }
+            }})
+                .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                callBack.methodToCallBack(null);
+            }
+        });
+    }
+
+    /**
+     * Devuelve la lista de facultades almacenadas en la base de datos
+     * @param callBack CallBack a ejecutar
+     */
+    public static void getFacultades(CallBack callBack) {
+        db.collection("facultades").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) { // Si se ha logrado recolectar todos los documentos de las facultades
+                            List<String> facultades = new ArrayList<String>();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                // Para cada documento obtenemos su id (nombre de la facultad)
+                                facultades.add(document.getId());
+                            }
+                            callBack.methodToCallBack(facultades);
+                        }
+                        else {
+                            callBack.methodToCallBack(null);
+                        }
+                    }})
+                .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                callBack.methodToCallBack(null);
+            }
+        });
+    }
+
+    /**
+     * Devuelve la lista de carreras que se dan en una facultad específica
+     * @param facultad Facultad por la que se quiere filtrar
+     * @param callBack CallBack a ejecutar
+     */
+    public static void getCarrerasByFacultad(String facultad, CallBack callBack) {
+        // Buscamos el documento para esa facultad
+        db.collection("facultades").document(facultad).get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()) { // Si existe la facultad
+                    callBack.methodToCallBack(documentSnapshot.getData().get("carreras"));
+                }
+                else { // Si no existe la facultad mostramos valor por defecto
+                    callBack.methodToCallBack(null);
+                }
+            }})
+                .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                callBack.methodToCallBack(null);
+            }
+        });
+    }
+
+    /**
+     * Devuelve las preferencias de un usuario concreto
      * @param email Email del usuario
      */
     public static void getPreferencesByEmail(String email, CallBack callBack) {
@@ -279,64 +328,9 @@ public class Firebase {
     }
 
     /**
-     * Crea un usuario en la base de datos (NO EN EL SERVICIO DE AUTENTIFICACIÓN)
-     * @param user Usuario a crear
-     * @param callBack CallBack a ejecutar, recibirá true si no hubo errores  o
-     * false si hubo algún error.
-     */
-    public static void createUser(User user, Preferences preferences, String uriFoto, CallBack callBack) {
-
-        // Create a new user
-        Map<String, Object> userParams = new HashMap<>();
-        userParams.put("nombre", user.getNombre());
-        userParams.put("apellidos", user.getApellidos());
-        userParams.put("fechaNacimiento", user.getFechaNacimiento());
-        userParams.put("sexo", user.getSexo());
-        userParams.put("facultad", user.getFacultad());
-        userParams.put("carrera", user.getCarrera());
-        userParams.put("sobreMi", user.getSobreMi());
-        userParams.put("contacto", user.getFormaContacto());
-
-        // Create preferences for user
-        Map<String, Object> userPreferences = new HashMap<>();
-        userPreferences.put("edadMinima", preferences.getEdadMinima());
-        userPreferences.put("edadMaxima", preferences.getEdadMaxima());
-        userPreferences.put("sexoBusqueda", preferences.getSexos());
-        userPreferences.put("facultad", preferences.getFacultad());
-        userPreferences.put("carrera", preferences.getCarrera());
-
-        // Añadimos al usuario
-        db.collection("usuarios").document(user.getEmail()).set(userParams)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void unused) {
-                db.collection("preferencias").document(user.getEmail()).set(userPreferences)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void unused) {
-                        uploadImage(Uri.parse(uriFoto));
-                        callBack.methodToCallBack(true);
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        callBack.methodToCallBack(false);
-                    }
-                });
-            }})
-                .addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                callBack.methodToCallBack(false);
-            }
-        });
-    }
-
-    /**
-     * Actualiza las preferencias de un usuario
+     * Actualiza las preferencias de un usuario concreto
      * @param email Email del usuario al que queremos cambiarle las preferencias
-     * @param callBack CallBack a ejecutar, recibirá true si no hubo errores  o
-     * false si hubo algún error.
+     * @param callBack CallBack a ejecutar
      */
     public static void updatePreferences(String email, Preferences preferences, CallBack callBack) {
 
@@ -360,13 +354,14 @@ public class Firebase {
         });
     }
 
+    // ---- Interactuar directamente con el módulo multimedia ----
+
     /**
      * Sube una foto al storage
      * @param uri Uri donde está la imagen
      */
     public static void uploadImage(Uri uri) {
         StorageReference filePath = storage.child("images").child(getUsuarioActual().getEmail());
-
         filePath.putFile(uri);
     }
 

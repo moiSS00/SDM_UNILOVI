d47422c533832ea4bb9b1c82da6ed6d65e4300a4
uo271397 <uo271397@uniovi.es>
2022-01-13 00:35:46 +0100

    Solucionado bug de pasar de pretendiente al minimizar aplicación y ocultar imagen al no haber pretendientes

--- app/src/main/java/com/example/unilovi/fragments/HomeFragment.java
+++ app/src/main/java/com/example/unilovi/fragments/HomeFragment.java
@@ -168,12 +168,15 @@ public class HomeFragment extends Fragment {
                                 layoutBotones.setVisibility(View.VISIBLE);
                                 layoutInfo.setVisibility(View.VISIBLE);
                                 pretendientes = (ArrayList<User>) object;
+                                indicePretendiente = 0;
                                 getNextPretendiente();
                             }
                             else {
                                 // Se cargaria una imagen por defecto
                                 layoutBotones.setVisibility(View.INVISIBLE);
                                 layoutInfo.setVisibility(View.INVISIBLE);
+                                imagenPretendiente.setVisibility(View.INVISIBLE);
+                                //Picasso.get().load(R.drawable.default_user_image).fit().into(imagenPretendiente);
                                 Util.showAlert(getContext(), "No hay usuarios que coincidan con tus preferencias");
                             }
                         }
